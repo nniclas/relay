de941e28a7135c1131848e82398705726c473cba
nniclas <niclas.oes@gmail.com>
2024-09-30 22:52:12 +0200

    popping sliding messages

--- app/src/components/v2/message-item.tsx
+++ app/src/components/v2/message-item.tsx
@@ -15,15 +15,28 @@ export const MessageItem = (a: MessageArgs) => {
                 height: 64,
                 width: 256,
                 display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                background: 'rgba(0,0,0,0.2)',
+                overflow: 'hidden',
+
+                // background: 'rgba(0,0,0,0.2)',
             }}
-            initial={{ y: -100, opacity: 0 }}
+            initial={{ y: -64, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             exit={{ opacity: 0 }}
         >
-            <div style={{ flex: 0 }}>Test {a.message.text}</div>
+            <motion.div
+                style={{
+                    display: 'flex',
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    background: 'rgba(0,40,0,0.2)',
+                }}
+                initial={{ x: -256 }}
+                animate={{ x: [-256, -128, 0] }}
+                exit={{ x: -256 }}
+            >
+                <div style={{ flex: 1 }}>Test {a.message.text}</div>
+            </motion.div>
         </motion.div>
     )
 }
